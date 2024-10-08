f805ea42201a9db21c6cdd48a96853c3aef1efd6
Sebastian Cervantes <scervantes3@my.nl.edu>
2024-10-08 14:46:25 -0500

    Add files via upload

---
+++ problem4.py
@@ -0,0 +1,7 @@
+#SebastianC
+#Oct 8,2024
+miles = int(input("enter miles driven: "))
+gallons = int(input("enter gallons used: "))
+miles_per_gallon=miles/gallons
+print("your MPG is: ", miles_per_gallon)
+#Calculating the MPG for a car 
