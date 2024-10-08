85cc5597e2c15a70ffab517ecfd03a81d96574ec
Sebastian Cervantes <scervantes3@my.nl.edu>
2024-10-08 14:34:19 -0500

    Add files via upload

---
+++ problem3.py
@@ -0,0 +1,9 @@
+#SebastianC
+#Oct 8, 2024
+Pi = int(3.14)
+radius = float(input("enter the radius of the circle: "))
+area_of_the_circle = Pi*radius*radius
+print ("Horray, your circle's area is:", area_of_the_circle)
+
+
+#Find the area of a circle with the radius imputted by user
