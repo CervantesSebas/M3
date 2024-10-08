cc78387f602e7c715f7eb22914b0eca6cccc10c2
Sebastian Cervantes <scervantes3@my.nl.edu>
2024-10-08 14:23:13 -0500

    Add files via upload

---
+++ Prob2a.py
@@ -0,0 +1,12 @@
+#SebastianC
+#Oct 8, 2024
+username = input ("Name:")
+user1 = "Sebastian"
+user2 = "Benjamin"
+if username == user1:
+    print("Access granted")
+elif username == user2:
+    print("Access granted")
+else:
+    print("Unauthorized user")
+#Only I and the instructor are allowed entry others are denied 
